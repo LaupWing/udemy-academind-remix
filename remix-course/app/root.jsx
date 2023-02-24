0646ad3320344abd2eef84bd5eb95b610d3294b4
LaupWing <loc_strict@hotmail.com>
2023-02-24 07:26:07 +0100

    udpate

--- remix-course/app/root.jsx
+++ remix-course/app/root.jsx
@@ -1,4 +1,5 @@
 import {
+   Link,
    Links,
    LiveReload,
    Meta,
@@ -27,6 +28,31 @@ export default function App() {
                <MainNavigation />
             </header>
             <Outlet />
+            <ScrollRestoration />
+            <Scripts />
+            <LiveReload />
+         </body>
+      </html>
+   )
+}
+
+export function ErrorBoundary({ error }) {
+   return (
+      <html lang="en">
+         <head>
+            <Meta />
+            <Links />
+            <title>An error occured!</title>
+         </head>
+         <body>
+            <header>
+               <MainNavigation />
+            </header>
+            <main className="error">
+               <h1>An error ocurred!</h1>
+               <p>{ error.message }</p>
+               <p>Back to safety <Link to={"/"}>safety</Link>!</p>
+            </main>
             <ScrollRestoration />
             <Scripts />
             <LiveReload />
