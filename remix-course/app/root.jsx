3d93a3f8395f79d89d5de774bf2bfc40c4cb6d62
LaupWing <loc_strict@hotmail.com>
2023-02-24 06:41:10 +0100

    nav link

--- remix-course/app/root.jsx
+++ remix-course/app/root.jsx
@@ -6,6 +6,7 @@ import {
    Scripts,
    ScrollRestoration,
 } from "@remix-run/react"
+import MainNavigation from "~/components/MainNavigation"
 import styles from "./styles/main.css"
 
 export const meta = () => ({
@@ -22,6 +23,9 @@ export default function App() {
             <Links />
          </head>
          <body>
+            <header>
+               <MainNavigation />
+            </header>
             <Outlet />
             <ScrollRestoration />
             <Scripts />
