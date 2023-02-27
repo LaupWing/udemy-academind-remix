eb6b922d4d64ad44bb3deedba543876cc01a8d15
LaupWing <loc_strict@hotmail.com>
2023-02-27 06:04:59 +0100

    udpate

--- remix-course/app/routes/notes.$noteId.jsx
+++ remix-course/app/routes/notes.$noteId.jsx
@@ -1,8 +1,12 @@
-import { Link } from "@remix-run/react"
+import { json } from "@remix-run/node"
+import { Link, useLoaderData } from "@remix-run/react"
 
 import styles from "~/styles/note-details.css"
+import { getStoredNotes } from "../data/notes"
 
 const NoteDetailsPage = () => {
+   const note = useLoaderData()
+
    return (
       <main id="note-details">
          <header>
@@ -11,11 +15,24 @@ const NoteDetailsPage = () => {
             </nav>
             <h1>NOTE TITLE</h1>
          </header>
-         <p id="note-details-content">NOTE CONTENT</p>
+         <p id="note-details-content">{note.content}</p>
       </main>
    )
 }
 export default NoteDetailsPage
+
+export function loader({params}) {
+   const notes = getStoredNotes()
+   const selectedNotes = notes.find(note => note.id === params.noteId)
+
+   if(!selectedNotes){
+      throw json({
+         message: "Could not find not or id " + params.nodeId
+      })
+   }
+
+   return selectedNotes
+}
 
 export function links() {
    return [
