af73d41fc625950ec9ade159f902fac75ec0ec15
LaupWing <loc_strict@hotmail.com>
2023-02-27 07:18:51 +0100

    update

--- expenses/app/routes/expenses.jsx
+++ expenses/app/routes/expenses.jsx
@@ -1,12 +1,31 @@
 import { Outlet } from "@remix-run/react"
 import expensesStyles from "~/styles/expenses.css"
+import ExpensesList from "../components/expenses/ExpensesList"
+
+const DUMMY_EXPENSES = [
+   {
+      id: "e1",
+      title: "First expense",
+      amount: 12.99,
+      date: new  Date().toISOString()
+   },
+   {
+      id: "e2",
+      title: "Second expense",
+      amount: 16.99,
+      date: new  Date().toISOString()
+   },
+]
+
 
 const ExpensesLayout = () => {
    return (
-      <main>
-         <p>Shared element</p>
+      <>
          <Outlet />
-      </main>
+         <main>
+            <ExpensesList expenses={DUMMY_EXPENSES}/>
+         </main>
+      </>
    )
 }
 export default ExpensesLayout
