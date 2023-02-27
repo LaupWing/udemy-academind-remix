e98bcb7a611b41094c488940305b8ca34d0e0aad
LaupWing <loc_strict@hotmail.com>
2023-02-27 07:06:38 +0100

    udpate

--- expenses/app/routes/expenses/analysis.jsx
+++ expenses/app/routes/expenses/analysis.jsx
@@ -1,4 +1,27 @@
+import ExpenseStatistics from "~/components/expenses/ExpenseStatistics"
+import Chart from "~/components/expenses/Chart"
+
+const DUMMY_EXPENSES = [
+   {
+      id: "e1",
+      title: "First expense",
+      amount: 12.99,
+      date: new  Date().toISOString()
+   },
+   {
+      id: "e2",
+      title: "Second expense",
+      amount: 16.99,
+      date: new  Date().toISOString()
+   },
+]
+
 const ExpensesAnalysisPage = () => {
-   return <div>ExpensesAnalysisPage</div>
+   return (   
+      <main>
+         <Chart expenses={DUMMY_EXPENSES}/>
+         <ExpenseStatistics expenses={DUMMY_EXPENSES}/>
+      </main>
+   )
 }
 export default ExpensesAnalysisPage
