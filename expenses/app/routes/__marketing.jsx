659b6b2add7864f677f75ae49eb42b89da859c53
LaupWing <loc_strict@hotmail.com>
2023-02-28 06:13:54 +0100

    update

---
+++ expenses/app/routes/__marketing.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "@remix-run/react"
+import marketingStyles from "~/styles/marketing.css"
+
+export default function MarketingLayout(){
+   return (
+      <Outlet />
+   )
+}
+
+export function links(){
+   return [
+      {
+         rel: "stylesheet",
+         href: marketingStyles
+      }
+   ]
+}
