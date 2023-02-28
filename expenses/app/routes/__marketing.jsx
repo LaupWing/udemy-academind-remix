e78452f67711687e8375f986b73fddfa406e4ea4
LaupWing <loc_strict@hotmail.com>
2023-02-28 06:17:28 +0100

    update

--- expenses/app/routes/__marketing.jsx
+++ expenses/app/routes/__marketing.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "@remix-run/react"
 import marketingStyles from "~/styles/marketing.css"
+import MainHeader from "~/components/navigation/MainHeader"
 
 export default function MarketingLayout(){
    return (
-      <Outlet />
+      <>
+         <MainHeader />
+         <Outlet />
+      </>
    )
 }
 
