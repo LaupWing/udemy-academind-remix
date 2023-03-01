b57bac175b19b9f4c630b74c05c89ffd7a29a501
LaupWing <loc_strict@hotmail.com>
2023-03-01 06:13:24 +0100

    udpate

--- expenses/app/routes/__app/expenses/$id.jsx
+++ expenses/app/routes/__app/expenses/$id.jsx
@@ -1,8 +1,8 @@
-import { json } from "@remix-run/node"
+// import { json } from "@remix-run/node"
 import { useNavigate } from "@remix-run/react"
 import ExpenseForm from "~/components/expenses/ExpenseForm"
 import Modal from "~/components/util/Modal"
-import { getExpense } from "~/data/expenses.server"
+// import { getExpense } from "~/data/expenses.server"
 
 const UpdateExpensesPage = () => {
    const navigate = useNavigate()
@@ -19,9 +19,9 @@ const UpdateExpensesPage = () => {
 }
 export default UpdateExpensesPage
 
-export async function loader({ params }) {
-   const expenseId = params.id
-   const expense = await getExpense(expenseId)
+// export async function loader({ params }) {
+//    const expenseId = params.id
+//    const expense = await getExpense(expenseId)
    
-   return json(expense)
-}
+//    return json(expense)
+// }
