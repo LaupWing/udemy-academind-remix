083e569e524cb85e6edfd208a2d6ccc53734f868
LaupWing <loc_strict@hotmail.com>
2023-03-01 07:09:43 +0100

    78

--- expenses/app/routes/__app/expenses.jsx
+++ expenses/app/routes/__app/expenses.jsx
@@ -6,6 +6,8 @@ import { getExpenses } from "~/data/expenses.server"
 const ExpensesLayout = () => {
    const expenses = useLoaderData()
 
+   const hasExpenses = expenses && expenses.length > 0 
+
    return (
       <>
          <Outlet />
@@ -21,13 +23,32 @@ const ExpensesLayout = () => {
                </a>
             </section>
 
-            <ExpensesList expenses={expenses}/>
+            {hasExpenses && <ExpensesList expenses={expenses}/>}
+            {!hasExpenses && (
+               <section id="no-expenses">
+                  <h1>No expenses found</h1>
+                  <p>Start <Link to={"add"}>adding some</Link> today.</p>
+               </section>
+            )}
          </main>
       </>
    )
 }
 export default ExpensesLayout
 
-export function loader() { 
-   return getExpenses()
+export async function loader() { 
+   const expenses = await getExpenses() 
+   return expenses
+   // if(!expenses || expenses.length === 0){
+   //    throw json({
+   //       message: "Could not find any expenses"
+   //    },{
+   //       status: 404,
+   //       statusText: "No expenses found"
+   //    })
+   // }
 }
+
+// export function CatchBoundary() {
+//    return
+// }
