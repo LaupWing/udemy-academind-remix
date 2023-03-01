5cea4d309c1f82efc3eabc1a91afdb814f7b2e95
LaupWing <loc_strict@hotmail.com>
2023-03-01 06:58:50 +0100

    76

--- expenses/app/root.jsx
+++ expenses/app/root.jsx
@@ -1,13 +1,16 @@
 import {
+   Link,
    Links,
    LiveReload,
    Meta,
    Outlet,
    Scripts,
    ScrollRestoration,
+   useCatch,
 } from "@remix-run/react"
 
 import sharedStyles from "~/styles/shared.css"
+import Error from "./components/util/Error"
 
 export const meta = () => ({
    charset: "utf-8",
@@ -15,31 +18,49 @@ export const meta = () => ({
    viewport: "width=device-width,initial-scale=1",
 })
 
-export default function App() {
+function Document ({title, children}) {
    return (
       <html lang="en">
          <head>
+            <title>{ title }</title>
             <Meta />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link
-               rel="preconnect"
-               href="https://fonts.gstatic.com"
-               crossOrigin="true"
-            />
-            <link
-               href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap"
-               rel="stylesheet"
-            />
             <Links />
          </head>
          <body>
-            <Outlet />
+            {children}
             <ScrollRestoration />
             <Scripts />
             <LiveReload />
          </body>
       </html>
    )
+}
+
+export default function App() {
+   return (
+      <Document>
+         <Outlet />
+      </Document>
+   )
+}
+
+export function CatchBoundary() {
+   const caughtResponse = useCatch()
+
+   return (
+      <Document title={caughtResponse.statusText}>
+         <main>
+            <Error>
+               <p>{caughtResponse.data?.message || "Something went wrong. Please try again later."}</p>
+               <p>Back to <Link to={"/"}>Safety</Link> </p>
+            </Error>
+         </main>
+      </Document>
+   )
+}
+
+export function ErrorBoundary() {
+
 }
 
 export function links() {
