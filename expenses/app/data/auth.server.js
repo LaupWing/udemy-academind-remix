842b4beb20be4b937a55ff46ad26c5e775cd2c84
LaupWing <loc_strict@hotmail.com>
2023-03-02 08:39:00 +0100

    92

--- expenses/app/data/auth.server.js
+++ expenses/app/data/auth.server.js
@@ -1,48 +1,65 @@
-import { hash } from "bcryptjs";
-import { prisma } from "./database.server";
+import { createCookieSessionStorage } from "@remix-run/node"
+import { hash } from "bcryptjs"
+import { prisma } from "./database.server"
 
-export async function signup({email, password}) {
+const SESSION_SECRET = process.env.SESSION_SECRET
+
+const sessionStorage = createCookieSessionStorage({
+   cookie: {
+      secure: process.env.NODE_ENV === "production",
+      secrets: [SESSION_SECRET],
+      sameSite: "lax",
+      maxAge: 30 * 24 * 60 * 60,
+      httpOnly: true
+   }
+})
+
+export async function signup({ email, password }) {
    const existingUser = await prisma.user.findFirst({
       where: {
-         email
-      }
+         email,
+      },
    })
 
-   if(existingUser){
-      const error = new Error("A user with the provided email address exists already")
+   if (existingUser) {
+      const error = new Error(
+         "A user with the provided email address exists already"
+      )
       error.status = 422
       throw error
    }
-   
+
    const passwordHash = await hash(password, 12)
 
    await prisma.user.create({
       data: {
          email,
-         password: passwordHash
-      }
+         password: passwordHash,
+      },
    })
 }
 
-export async function login({email, password}){
+export async function login({ email, password }) {
    const existingUser = await prisma.user.findFirst({
       where: {
-         email
-      }
+         email,
+      },
    })
 
-   if(!existingUser){
-      const error = new Error("Could not log you in, please check the provided credentials")
+   if (!existingUser) {
+      const error = new Error(
+         "Could not log you in, please check the provided credentials"
+      )
       error.status = 401
       throw error
    }
 
    const passwordCorrect = await compare(password, existingUser.password)
-   if(!passwordCorrect){
-      const error = new Error("Could not log you in, please check the provided credentials")
+   if (!passwordCorrect) {
+      const error = new Error(
+         "Could not log you in, please check the provided credentials"
+      )
       error.status = 401
       throw error
    }
-
-   
 }
